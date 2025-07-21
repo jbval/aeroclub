c49cc24f7bcb88c7ca5cc9eb682946899f06872c
jbval <jbval2@gmail.com>
2025-07-21 21:48:17 +0200

    feat: update ng cli 19

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,11 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { register } from 'swiper/element/bundle';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    selector: 'app-home',
+    imports: [],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent implements OnInit {
   constructor() {
--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-inscription',
-  standalone: true,
-  imports: [],
-  templateUrl: './inscription.component.html',
-  styleUrl: './inscription.component.scss'
+    selector: 'app-inscription',
+    imports: [],
+    templateUrl: './inscription.component.html',
+    styleUrl: './inscription.component.scss'
 })
 export class InscriptionComponent {
 
