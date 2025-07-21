8a7b7114592c496a8b42085dd63e7739a1e950e4
jbval <jbval2@gmail.com>
2025-07-21 21:42:09 +0200

    feat: add swipper

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class HomeComponent implements OnInit {
+  constructor() {
+    register();
+  }
+  ngOnInit(): void {}
+}
